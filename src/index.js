bff2201d68bd35238aab2e84edde8638259a1fec
Donadonzi <donadonzi@gmail.com>
2020-05-09 19:23:33 -0400

    building front-end, login page

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 const express = require('express');
 require('./db/mongoose');
+const path = require('path');
 
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 
+
 const app = express();
 const port = process.env.PORT;
 
-app.listen(port, () => {
-	console.log('Yallah! Server umad bala roo port ' + port);
-});
+const publicDirectory = path.join(__dirname, '../public');
+app.use(express.static(publicDirectory));
 
 
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
+
+
+app.listen(port, () => {
+	console.log('Yallah! Server umad bala roo port ' + port);
+});
+
 
