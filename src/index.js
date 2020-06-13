11040e3de9bcca812daae89f976d1f6d61e744ef
Donadonzi <donadonzi@gmail.com>
2020-06-13 14:11:30 -0400

    replaced heroku config. removed statc.json.SHOULD WORK HOPEFULLY

--- src/index.js
+++ src/index.js
@@ -8,8 +8,9 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT;
 
-// const publicDirectory = path.join(__dirname, '../client/public');
-// app.use(express.static(publicDirectory));
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
 
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
@@ -18,10 +19,6 @@ if (process.env.NODE_ENV === 'production') {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
 }
-
-app.use(express.json());
-app.use(userRouter);
-app.use(taskRouter);
 
 app.listen(port, () => {
 	console.log('Server is up on port ' + port);
